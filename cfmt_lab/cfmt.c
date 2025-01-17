87cee20a4271e74d304e677c718f298c3796bd28
Marcus Shaffer <2003mashaffer@gmail.com>
2025-01-17 11:01:49 -0500

    Update cfmt.c

--- cfmt_lab/cfmt.c
+++ cfmt_lab/cfmt.c
@@ -14,10 +14,17 @@ int main(int argc, char **argv) {
   /*
    * Load input file into buf. Note that file size is limited to 2^16 bytes/
    * Additional data will be truncated
+   * Buf is array for test.c with preallocated 65Kb of space
    */
   char buf[65535];
+  /**
+   * Reading the file into src
+   */
   int src = open(argv[1], O_RDONLY);
-  read(src, buf, 65535);
+  /**
+   * Length of file read
+   */
+  int len = read(src, buf, 65535);
   close(src);
 
   /*
@@ -28,7 +35,50 @@ int main(int argc, char **argv) {
   /*
    * TODO: Write formatted file to output
    */
+  int count = 0;
 
+  /**
+   * The 4 cases 
+   *  1.) If there is a \t or 9 in hex replace it with 4 spaces or 20 in hex
+   *  2.) If there is a \r\n (or da in hex)or just \r (or d in hex) replace it with \n or just a in hex
+   *  3.) If there is a space or 20 next to an \n or a, remove the space or 20
+   *  4.) If a curly bracket 7b comes after an \n or a, change it to one space or 20
+   */
+
+  while( count < len){
+
+    /**
+     * Replacess the hex 9 with 4 spaces
+     * For this section look at useing pointers to update 
+     */
+    if(buf[count] == '\t'){
+      buf[count] = ' '; // Needs to be added 4 times
+      
+    }
+
+
+    /** 
+     * Replae \r with a \n and if the following charcater is a \n then delete it
+     * Come back to this when there is a \r
+     */
+
+    if(buf[count] == '\r' && buf[count+1] == '\n'){
+      buf[count] = '\0';
+    }else if(buf[count] == '\r'){
+      buf[count] = '\n';
+    }
+
+    if((buf[count] == ' ' && buf[count + 1] == '\n') || buf[count] == '\r'){
+      for(int i = count; i < len; i++){
+        buf[i] = buf[i+1];
+      }
+      len--;
+    }
+  printf("%x\n", buf[count]);
+    count++;
+  }
+
+   write(dst,buf,len);
   /*
    * Close output file
    */
