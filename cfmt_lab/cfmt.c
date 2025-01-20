47b7ae72b945a72c6677da4510aaedf710aded08
shama2025 <2003mashaffer@gmail.com>
2025-01-20 10:07:01 -0500

    Update cfmt.c

--- cfmt_lab/cfmt.c
+++ cfmt_lab/cfmt.c
@@ -16,8 +16,9 @@ int main(int argc, char **argv) {
    * Additional data will be truncated
    */
   char buf[65535];
+  char newBuf[65535];
   int src = open(argv[1], O_RDONLY);
-  read(src, buf, 65535);
+  int len = read(src, buf, 65535);
   close(src);
 
   /*
@@ -29,6 +30,21 @@ int main(int argc, char **argv) {
    * TODO: Write formatted file to output
    */
 
+   /**
+   * The 4 cases 
+   *  1.) If there is a \t or 9 in hex replace it with 4 spaces or 20 in hex
+   *  2.) If there is a \r\n (or da in hex)or just \r (or d in hex) replace it with \n or just a in hex
+   *  3.) If there is a space or 20 next to an \n or a, remove the space or 20
+   *  4.) If a curly bracket 7b comes after an \n or a, change it to one space or 20
+   */
+
+  for (int i = 0; i < len; i++)
+  {
+    
+  }
+  
+  
+  write(dst, buf, len);
   /*
    * Close output file
    */
