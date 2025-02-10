b426173ad2628edc97523fbe01264a25ba4bd1ff
shama2025 <2003mashaffer@gmail.com>
2025-02-10 15:43:53 +0000

    Update sum.c

--- multithread_lab/sum.c
+++ multithread_lab/sum.c
@@ -4,24 +4,21 @@
 
 #define SIZE 1600000000
 #define THREADS 4
-#define ADDEND 399999999
 
 /**
+ * @brief Using a range of values computes a running sum
+ * @param num The nth thread
+ * @return The running sum
  */
 static void* sum(void *num){
   long scalar = SIZE/THREADS; // The amount of work this thread wil do
   long start = (*(long*)num *scalar) + 1; // The starting point for the loop
-  long end = start+ADDEND; // The ending point for the loop
-  long run_sum; // Running sum
+  long end = start+scalar; // The ending point for the loop
+  long run_sum = 0; // Running sum
 
-  printf("Child thread has a starting point of: %ld\n", start);
-  printf("Child thread has a scalar of: %ld\n", scalar);
-  printf("Child thread has a ending point of: %ld\n", end);
   for(int i = start; i<end; i++){
     run_sum+=i;
   }
-
-  printf("Child thread has a running sum of %ld\n",run_sum);
 
   return (void*)run_sum;
 }
@@ -43,10 +40,10 @@ int main(){
     count++;
   }while(count < THREADS);
 
-  for(int i = 0; i<= THREADS; i++){
+  for(int i = 0; i< THREADS; i++){
     pthread_join(child_thread[i], (void*)&col_sum[i]);
     total+=col_sum[i];
   }
-  printf("This is the output from the threads: %lld\n", total);
-  return 0;
+  //printf("This is the output from the threads: %lld\n", total);
+  return total;
 }
