c15a74040a426f8cdf75ade04b9d05753ded2170
shama2025 <2003mashaffer@gmail.com>
2025-02-10 15:44:33 +0000

    Format sum.c using built in formatter

--- multithread_lab/sum.c
+++ multithread_lab/sum.c
@@ -10,40 +10,45 @@
  * @param num The nth thread
  * @return The running sum
  */
-static void* sum(void *num){
-  long scalar = SIZE/THREADS; // The amount of work this thread wil do
-  long start = (*(long*)num *scalar) + 1; // The starting point for the loop
-  long end = start+scalar; // The ending point for the loop
-  long run_sum = 0; // Running sum
+static void *sum(void *num)
+{
+  long scalar = SIZE / THREADS;             // The amount of work this thread wil do
+  long start = (*(long *)num * scalar) + 1; // The starting point for the loop
+  long end = start + scalar;                // The ending point for the loop
+  long run_sum = 0;                         // Running sum
 
-  for(int i = start; i<end; i++){
-    run_sum+=i;
+  for (int i = start; i < end; i++)
+  {
+    run_sum += i;
   }
 
-  return (void*)run_sum;
+  return (void *)run_sum;
 }
 
-int main(){
+int main()
+{
   pthread_t child_thread[THREADS]; // Threads
-  long inputs[THREADS ]; // The inputs passed to the function
-  int error; // File descriptor upon thread creation
-  int count = 0; // Index tracker
-  long col_sum[THREADS ]; // An array of values passed from thread
-  long long total = 0; // Total sum 
+  long inputs[THREADS];            // The inputs passed to the function
+  int error;                       // File descriptor upon thread creation
+  int count = 0;                   // Index tracker
+  long col_sum[THREADS];           // An array of values passed from thread
+  long long total = 0;             // Total sum
 
-  do{
+  do
+  {
     inputs[count] = count;
     error = pthread_create(&child_thread[count], NULL, sum, &inputs[count]);
-    if(error){
+    if (error)
+    {
       fprintf(stderr, "pthread_creat failed with error: %d\n", error);
     }
     count++;
-  }while(count < THREADS);
+  } while (count < THREADS);
 
-  for(int i = 0; i< THREADS; i++){
-    pthread_join(child_thread[i], (void*)&col_sum[i]);
-    total+=col_sum[i];
+  for (int i = 0; i < THREADS; i++)
+  {
+    pthread_join(child_thread[i], (void *)&col_sum[i]);
+    total += col_sum[i];
   }
-  //printf("This is the output from the threads: %lld\n", total);
   return total;
 }
