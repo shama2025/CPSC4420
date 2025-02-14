7cad9d50760e591d64b4459c4c06119f43deb085
shama2025 <2003mashaffer@gmail.com>
2025-02-14 15:56:27 +0000

    *Create backend.c *Update agent.py

--- multiprocessing_lab/agent.py
+++ multiprocessing_lab/agent.py
@@ -1,2 +1,23 @@
 # This file will simply create x amount of agents and send/return data from a backend
 
+import requests
+import threading
+
+def comm_backend(thread_num):
+    """
+    This function will send requests to the backend
+    """
+    
+
+def create_agents(agent_num):
+    """
+    This function will create x amount of threads
+    """
+    for i in range(0,agent_num):
+        threading.Thread(target=comm_backend, args=[i]).run()
+
+
+if __name__ == "__main__":
+    print("Starting program")
+    agent_num = int(input("How many agents would you like to create: "))
+    create_agents(agent_num)
---
+++ multiprocessing_lab/backend.c
@@ -0,0 +1,18 @@
+/**
+ * This file will feature a simple backend that will take network requests
+ * from an agent and put them into an array
+ *  This backend will need to utilize sockets, so building a basic socket
+ *  is needed
+ * 
+ * Once there are no more requests, the array will start iterating through and
+ * creating forks to handle the process
+ * 
+ * The data the agent needs will need to have the child process 
+ * that handled the requests
+ */
+
+
+
+int main(){
+    return 0;
+}
