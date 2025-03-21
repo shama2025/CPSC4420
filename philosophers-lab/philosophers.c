74c42736cab3dc019a44f5b58b4f6299c95c8084
shama2025 <2003mashaffer@gmail.com>
2025-03-21 14:28:42 +0000

    Create philosophers lab

---
+++ philosophers-lab/philosophers.c
@@ -0,0 +1,162 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <assert.h>
+
+#define HUNGER 0x10
+#define PHILOSOPHERS 0x10
+#define DELAY 0x1000000
+
+typedef struct
+{
+  pthread_mutex_t mutex;
+  unsigned int held;
+  unsigned int uses;
+  unsigned int times_held;
+} Chopstick;
+
+typedef struct
+{
+  Chopstick *left_chopstick;
+  Chopstick *right_chopstick;
+  unsigned int food_consumed;
+  unsigned int id;
+} Philosopher;
+
+void eat(Philosopher *philosopher)
+{
+  /*
+   * Simulates a philosopher eating. Requires both chopsticks to be held.
+   */
+
+  philosopher->food_consumed++;
+  printf("Philosopher %d has eaten %d times.\n", philosopher->id, philosopher->food_consumed);
+
+  assert(philosopher->left_chopstick->uses < philosopher->left_chopstick->times_held);
+  assert(philosopher->left_chopstick->held == 1);
+  assert(philosopher->right_chopstick->held == 1);
+  philosopher->left_chopstick->uses++;
+  philosopher->right_chopstick->uses++;
+}
+
+void get_chopstick(Chopstick *chopstick)
+{
+  /*
+   * Blocks until `chopstick` is available then picks it up
+   */
+  for (unsigned i = 0; i < DELAY; i++)
+    ;
+  pthread_mutex_lock(&chopstick->mutex);
+  assert(chopstick->held == 0);
+  chopstick->held = 1;
+  chopstick->times_held++;
+}
+
+void return_chopstick(Chopstick *chopstick)
+{
+  /*
+   * Puts `chopstick` back and never blocks
+   */
+  assert(chopstick->held == 1);
+  chopstick->held = 0;
+  pthread_mutex_unlock(&chopstick->mutex);
+}
+
+long int run_philosopher(Philosopher *philosopher)
+{
+  /*
+   * Implements one dining philosopher
+   *
+   * The purpose of this thread is to `eat` until no longer hungry. In order
+   * to eat, a philosopher must be holding their right and left chopstick. The
+   * naive solution to this problem creates a deadlock. You must find a
+   * better algorithm. The ideal algorithm will still allow concurrent
+   * eating without creating deadlocks.
+   *
+   * Note that philosophers must return chopsticks between calls to `eat`.
+   */
+
+  while (philosopher->food_consumed < HUNGER)
+  {
+    if (philosopher->id % 2 == 0)
+    {
+      get_chopstick(philosopher->left_chopstick);
+      get_chopstick(philosopher->right_chopstick);
+    }
+    else
+    {
+      get_chopstick(philosopher->right_chopstick);
+      get_chopstick(philosopher->left_chopstick);
+    }
+
+    eat(philosopher);
+
+    return_chopstick(philosopher->right_chopstick);
+    return_chopstick(philosopher->left_chopstick);
+  }
+
+  return philosopher->food_consumed;
+}
+
+/*
+ * Setup and test code
+ */
+
+int main()
+{
+  pthread_t child_thread[PHILOSOPHERS];
+
+  Philosopher philosophers[PHILOSOPHERS];
+  Chopstick chopsticks[PHILOSOPHERS];
+
+  for (unsigned long i = 0; i < PHILOSOPHERS; i++)
+  {
+    pthread_mutex_init(&chopsticks[i].mutex, 0);
+
+    chopsticks[i].held = 0;
+    chopsticks[i].uses = 0;
+    chopsticks[i].times_held = 0;
+
+    philosophers[i].id = i;
+    philosophers[i].food_consumed = 0;
+    philosophers[i].left_chopstick = &chopsticks[i];
+    philosophers[i].right_chopstick = &chopsticks[(i + 1) % PHILOSOPHERS];
+
+    int code;
+    code = pthread_create(&child_thread[i], NULL, (void *)run_philosopher, (void *)&philosophers[i]);
+    if (code)
+    {
+      fprintf(stderr, "pthread_create failed with code %d\n", code);
+    }
+  }
+
+  unsigned long thread_consumed;
+  unsigned long reported_total_consumed = 0;
+
+  for (unsigned int i = 0; i < PHILOSOPHERS; i++)
+  {
+    pthread_join(child_thread[i], (void *)&thread_consumed);
+    assert(thread_consumed == HUNGER);
+    assert(philosophers[i].food_consumed == HUNGER);
+    reported_total_consumed += thread_consumed;
+  }
+
+  printf("%ld total food consumed.\n", reported_total_consumed);
+  assert(reported_total_consumed == HUNGER * PHILOSOPHERS);
+
+  unsigned long total_chopstick_uses = 0;
+  unsigned long total_chopstick_holds = 0;
+
+  for (unsigned int i = 0; i < PHILOSOPHERS; i++)
+  {
+    total_chopstick_uses += chopsticks[i].uses;
+    total_chopstick_holds += chopsticks[i].times_held;
+  }
+
+  printf("%ld total chopstick uses.\n", total_chopstick_uses);
+  assert(total_chopstick_uses == HUNGER * PHILOSOPHERS * 2);
+  assert(total_chopstick_holds == HUNGER * PHILOSOPHERS * 2);
+
+  printf("All tests passed.\n");
+
+  return 0;
+}
---
+++ philosophers-lab/readme.md
@@ -0,0 +1,28 @@
+Dining Philosophers
+===================
+
+> The dining philosophers problem is an example problem often used in concurrent algorithm design to illustrate synchronization issues and techniques for resolving them. It was originally formulated in 1965 by Edsger Dijkstra as a student exam exercise, presented in terms of computers competing for access to tape drive peripherals.
+
+![Dining Philosopers](philosophers.png)
+
+This lab provides an opportunity to explore the dining philosophers problem. You task is to provide an implementation for the concurrent `run_philosopher` routine such that philosophers are able to concurrently eat successfully.
+
+Problem
+-------
+
+> A number of philosophers live in a house where the table is laid for them; each philosopher has their own place at the table. Their only problem – besides those of philosophy – is that the dish served is a very difficult kind of spaghetti, that has to be eaten with two chopsticks. There are two chopsticks next to each plate, so that presents no difficulty: as a consequence, however, no two neighbours may be eating simultaneously.
+>
+> A philosopher can only eat spaghetti when they have both left and right chopsticks. Each chopstick can be held by only one philosopher at a time and so a philosopher can use the chopstick only if it is not being used by another philosopher. After an individual philosopher finishes eating, they need to put down both chopsticks so that the chopsticks become available to others. A philosopher can only take the chopstick on their right or the one on their left as they become available, and they cannot start eating before getting both chopsticks.
+
+A more detailed description of the problem and possible solutions is available on [Wikipedia](https://en.wikipedia.org/wiki/Dining_philosophers_problem). 
+
+Running
+-------
+
+An example program, `philisophers.c` is included. It contains a stub implementation that leads to a deadlock. You can run and test the program by calling:
+
+```
+make test
+```
+
+You will likely notice the program stall due to the deadlock. A successful algorithm with allow the program to run until all philosophers are finished eating. Philosophers must eat concurrently. It is not acceptable for a few philiosphers to eat until satisfied before handing over their chopsticks.
