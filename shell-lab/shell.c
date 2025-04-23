3221e8dd58e1af8374ebdfffc49417037d4131ce
Marcus Shaffer <101298098+shama2025@users.noreply.github.com>
2025-04-23 18:52:31 -0400

    Update shell.c

--- shell-lab/shell.c
+++ shell-lab/shell.c
@@ -112,7 +112,7 @@ void _start() {
   // Read-eval-print loop
   while (1) {
     // TODO: Use `write` to display a prompt such as "> "
-    write(1,"write your command: ",22);
+    write(1,">",2);
     read(STDIN_FILENO, line, 1024);
     eval_and_print(line);
   }
